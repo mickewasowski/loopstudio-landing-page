8f37ef65657cc8154ffb0984ed5f29a27f142128
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2022-07-03 23:46:42 +0300

    implemented initial dashboard and navigation for mobile

--- function.js
+++ function.js
@@ -19,3 +19,16 @@ function removeBackdrop() {
         backdrop[0].style.display = 'none';
     }
 }
+
+function toggleNavigation() {
+    let navContainer = document.getElementById('navigation-container');
+    let logo = document.getElementById('mobile-logo');
+
+    if (navContainer.style.display === 'none') {
+        navContainer.style.display = "flex";
+        logo.style.display = 'flex';
+    } else if (navContainer.style.display === 'flex') {
+        navContainer.style.display = "none";
+        logo.style.display = 'none';
+    }
+}
